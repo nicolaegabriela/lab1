1a9699dbf7beed6bc165c0a9de20d5fefa5c58dc
Serban Chiricescu <serban.chiricescu@qualitance.com>
2015-07-06 00:04:17 +0300

    Initial Commit

---
+++ lab1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lab1
+//
+//  Created by Serban Chiricescu on 06/07/15.
+//  Copyright (c) 2015 Qualitance. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
